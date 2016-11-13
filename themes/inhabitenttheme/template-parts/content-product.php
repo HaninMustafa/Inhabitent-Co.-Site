24331ed7d095899c9887eb813f0fbb02deb536fb
HaninMustafa <haninmustafa13@gmail.com>
2016-11-13 14:48:19 -0800

    shop page styled

--- themes/inhabitenttheme/template-parts/content-product.php
+++ themes/inhabitenttheme/template-parts/content-product.php
@@ -7,23 +7,18 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
+
+<div class="img-wrap-product">
+  <a href="<?php echo get_permalink() ?>">
+    <?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
+  </a>
+</div>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<!-- <div class="entry-content">
-		<?php the_excerpt(); ?>
-
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+<div class="product-info">
+		<?php the_title('<h2 class="entry-title-product">', '</h2>' ); ?>
+<div class="price">
+    <?php echo CFS()->get( 'product_price' );?>
+  </div>
+  </div>
