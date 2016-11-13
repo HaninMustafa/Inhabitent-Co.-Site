9dd2c99f43876132a2bfc578b6be3fd519ba3024
HaninMustafa <haninmustafa13@gmail.com>
2016-11-13 15:21:56 -0800

    taxonomy product type fully styled

--- themes/inhabitenttheme/taxonomy-product-type.php
+++ themes/inhabitenttheme/taxonomy-product-type.php
@@ -14,11 +14,8 @@ get_header(); ?>
 <div class="container">
 
 			<header class="page-header-shop">
-				<h1 class="page-title">
         <?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-				</h1>
-					<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
