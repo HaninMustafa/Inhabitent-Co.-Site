e995dc64b2d3ff7944a685760492fbb2b8613301
HaninMustafa <haninmustafa13@gmail.com>
2016-11-13 17:35:51 -0800

    single product page styled

--- themes/inhabitenttheme/single-product.php
+++ themes/inhabitenttheme/single-product.php
@@ -7,19 +7,46 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+<div id="primary" class="site-main-shop">
 		<main id="main" class="site-main" role="main">
+<div class="container">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single-product' ); ?>
 
-      <?php echo CFS()->get( 'product_price' ); ?>
+			<article class="single-product-article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header id="single-product" class="entry-header single-product-img">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?>
+					<?php endif; ?>
+				</header><!-- .entry-header -->
+
+
+				<div class="">
+					<div class="single-product-title">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</div>
+
+				<div class="single-product-price">
+					<?php echo CFS()->get( 'product_price' ); ?>
+				</div>
+
+				<div class="single-product-content">
+					<?php the_content(); ?>
+				</div>
+
+					<div class="social-buttons">
+					   <a href="" type="" class="black-btn"><i class="fa fa-facebook"></i>Like</a>
+					   <a href="" type="" class="black-btn"><i class="fa fa-twitter"></i>Tweet</a>
+					   <a href="" type="" class="black-btn"><i class="fa fa-pinterest"></i>Pin</a>
+					</div>
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->
 
 		<?php endwhile; // End of the loop. ?>
 
+	</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
