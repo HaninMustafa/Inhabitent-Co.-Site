9832f08f85e78bcad90d46f47cd32bdacd42fa39
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-11-21 11:55:28 -0800

    changing gulp files to mach new localhost url + fixing fonts fallback

--- themes/inhabitenttheme/gulpfile.js
+++ themes/inhabitenttheme/gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('browser-sync', function() {
    ];
 
     browserSync.init(files, {
-        proxy: 'localhost/inhabitent/',
+        proxy: 'localhost/student/',
     });
 
     gulp.watch(files).on('change', browserSync.reload);
